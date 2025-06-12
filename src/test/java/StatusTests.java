f8ce9f718da5d23bc0864e90362f539ac0be23af
Tigr0K <yufing@mail.ru>
2025-06-12 14:36:34 +0300

    add tests

---
+++ src/test/java/StatusTests.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+
+
+public class StatusTests {
+    @Test
+    void checkTotal5() {
+        get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total", is(5))
+                .statusCode(200)
+                .body("browsers.chrome", hasKey("127.0"));
+    }
+
+    @Test
+    void checkTotalWithLogs() {
+        given()
+                .log().all()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().all()
+                .body("total", is(5));
+    }
+}
